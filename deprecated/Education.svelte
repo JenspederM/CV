4a7afaedd57447fdae29ea450807eb8d073ca327
Jens Peder Meldgaard <jenspederm@gmail.com>
2022-12-01 12:31:33 +0100

    move deprecated out of src

--- deprecated/Education.svelte
+++ deprecated/Education.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import Box from "../lib/common/Box.svelte";
+  import Box from "../src/lib/common/Box.svelte";
   import { slide } from "svelte/transition";
 
   enum EducationType {
